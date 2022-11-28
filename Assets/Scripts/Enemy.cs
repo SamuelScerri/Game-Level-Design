48101c68f6cce123b151ede63c52db4b7a7bbaf7
SamuelScerri <samuelscerri533@gmail.com>
2022-11-28 10:38:20 +0100

    Added Zombie AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int HP = 100;
+    public Animator animator;
+
+    public void TakeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+        if(HP <= 0)
+        {
+            //Play Death Animation
+            animator.SetTrigger("die");
+            GetComponent<Collider>().enabled = false;
+
+        }
+        else{
+            //Play Damage Animation
+            animator.SetTrigger("damage");
+        }
+    }
+}
