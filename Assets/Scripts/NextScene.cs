c0d7ee507c5dc893ff40bfd5221de5e6437f1a1a
KyleT-bone <kyle.tabone.e21475@mcast.edu.mt>
2022-12-19 15:22:37 +0100

    Target for player

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NextScene : MonoBehaviour
 {
     public GameObject nextSceneCollider;
+    public GameObject waypoint;
     public List<GameObject> enemy = new List<GameObject>();
     public GameObject[] enemyArray;
     // Start is called before the first frame update
@@ -13,6 +14,7 @@ public class NextScene : MonoBehaviour
         enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
         enemy.AddRange(enemyArray);
         nextSceneCollider.SetActive(false);
+        waypoint.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class NextScene : MonoBehaviour
         if(enemy.Count == 0)
         {
             nextSceneCollider.SetActive(true);
+            waypoint.SetActive(true);
         }
     }
 }
